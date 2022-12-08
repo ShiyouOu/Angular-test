11d081d436459d18ba84d90dc045a531e3dedf97
ShiyouOu <54868441+ShiyouOu@users.noreply.github.com>
2022-12-08 17:42:30 -0500

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,34 @@ export class AppComponent implements OnInit {
   products: any[] = [];
 
   ngOnInit() {
-    this.products.push(["M&M", "Snack", "$1.99"])
+    this.products.push(['M&M', 'Snack', '$1.99']);
+    this.products.push(['Table', 'Furniture', '$199']);
+    this.products.push(['Kale', 'Vegetables', '$2.49']);
+  }
+
+  clearFields() {
+    // Clear the input fields(after adding a new product)
+    (<HTMLInputElement>document.getElementById('product-name')).value = '';
+    (<HTMLInputElement>document.getElementById('product-category')).value = '';
+    (<HTMLInputElement>document.getElementById('product-price')).value = '';
+  }
+
+  addProduct() {
+    const productName = (<HTMLInputElement>(
+      document.getElementById('product-name')
+    )).value;
+    const productCategory = (<HTMLInputElement>(
+      document.getElementById('product-category')
+    )).value;
+    const productPrice = (<HTMLInputElement>(
+      document.getElementById('product-price')
+    )).value;
+
+    if (productName == '' || productCategory == '' || productPrice == '') {
+      return;
+    }
+
+    this.products.push([productName, productCategory, productPrice]);
+    this.clearFields();
   }
 }
--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: '[my-product]',
   templateUrl: './product.component.html',
   styleUrls: ['./app.component.css'],
 })
@@ -9,4 +9,6 @@ export class ProductComponent {
   @Input() productName: string;
   @Input() productCategory: string;
   @Input() productPrice: string;
+
+  deleteProduct() {}
 }
